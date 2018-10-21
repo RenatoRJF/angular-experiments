19d5195152af9f2b0422da5f7da61e930f7c0282
Renato <nato.rjf@gmail.com>
2018-10-21 16:47:40 +0100

    - Add static template in html - Customize the components - Create a simple repositories mock

---
+++ src/app/repository-mock.ts
@@ -0,0 +1,61 @@
+const repositories: Array<any> = [
+  {
+    "id": 24195339,
+    "name": "angular",
+    "full_name": "angular/angular",
+    "owner": {
+      "login": "angular",
+      "avatar_url": "https://avatars3.githubusercontent.com/u/139426?v=4",
+      "html_url": "https://github.com/angular"
+    },
+    "forks": 12000,
+    "description": "One framework. Mobile & desktop.",
+    "created_at": "2014-09-18T16:12:01Z",
+    "updated_at": "2018-10-21T13:32:02Z"
+  },
+  {
+    "id": 10270250,
+    "name": "react",
+    "full_name": "facebook/react",
+    "owner": {
+      "login": "facebook",
+      "avatar_url": "https://avatars3.githubusercontent.com/u/69631?v=4",
+      "html_url": "https://github.com/facebook"
+    },
+    "forks": 12000,
+    "description": "A declarative, efficient, and flexible JavaScript library for building user interfaces.",
+    "created_at": "2013-05-24T16:15:54Z",
+    "updated_at": "2018-10-21T15:12:21Z"
+  },
+  {
+    "id": 11730342,
+    "name": "vue",
+    "full_name": "vuejs/vue",
+    "owner": {
+      "login": "vuejs",
+      "avatar_url": "https://avatars1.githubusercontent.com/u/6128107?v=4",
+      "html_url": "https://github.com/vuejs"
+    },
+    "forks": 12000,
+    "description": "🖖 A progressive, incrementally-adoptable JavaScript framework for building UI on the web.",
+    "created_at": "2013-07-29T03:24:51Z",
+    "updated_at": "2018-10-21T15:10:39Z"
+  },
+  {
+    "id": 5532320,
+    "name": "polymer",
+    "full_name": "Polymer/polymer",
+    "owner": {
+      "login": "Polymer",
+      "avatar_url": "https://avatars3.githubusercontent.com/u/2159051?v=4",
+      "html_url": "https://github.com/Polymer"
+    },
+    "forks": 12000,
+    "description": "Build modern apps using web components",
+    "created_at": "2012-08-23T20:56:30Z",
+    "updated_at": "2018-10-21T11:22:16Z"
+  }
+];
+
+
+export default repositories;
